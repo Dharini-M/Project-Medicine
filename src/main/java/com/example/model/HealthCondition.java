1b6304a83b955d6f7d915c0bf384141b14014777
jeswanth881 <jeswanthk44@gmail.com>
2022-01-29 11:10:01 +0530

    jeswanth

--- src/main/java/com/example/model/HealthCondition.java
+++ src/main/java/com/example/model/HealthCondition.java
@@ -8,7 +8,11 @@ import java.util.Scanner;
 public class HealthCondition {
     Cart cart=new Cart();
     Scanner s=new Scanner(System.in);
+    HashMap<String,Integer> map1=new HashMap<>();
+    ArrayList<String> list=new ArrayList<>();
+    HashMap<String,Integer> map=new HashMap<>();
     int quantity;
+    int select;
     public void main(){
         System.out.println("select your choice:\n 1. Cold And Cough 2. Stomach Care 3. Eye Care 4. cart");
         int select=s.nextInt();
@@ -21,28 +25,40 @@ public class HealthCondition {
         }
     }
     public void coldAndCough(){
-        ArrayList<String> list=new ArrayList<>();
         list.add("Vicks Action 500");
         list.add("Paraceptamol");
         list.add("Citrizine");
-        HashMap<String,Integer> map=new HashMap<>();
         map.put("Vicks Action 500",5);
         map.put("Paraceptamol",2);
         map.put("Citrizine",1);
-        HashMap<String,Integer> map1=new HashMap<>();
         System.out.println("1. Vicks Action 500 2. Paraceptamol 3. Citrizine 4. Back 5. Exit");
         System.out.println("Enter your Choice");
-        int select=s.nextInt();
+        select=s.nextInt();
         if(select!=4 && select!=5) {
             System.out.println("Enter the quantity");
             quantity=s.nextInt();
         }
+        methodOverload();
+        System.out.println("good bye");
+    }
+
+    private void methodOverload() {
         do {
             if(select==5){
                 break;
             }
             switch (select) {
                 case 1:
+                    map1.put(list.get(select-1),quantity*(map.get(list.get(select-1))));
+                    cart.setMap(map1);
+                    main();
+                    break;
+                case 2:
+                    map1.put(list.get(select-1),quantity*(map.get(list.get(select-1))));
+                    cart.setMap(map1);
+                    main();
+                    break;
+                case 3:
                     map1.put(list.get(select-1),quantity*(map.get(list.get(select-1))));
                     cart.setMap(map1);
                     main();
@@ -56,6 +72,5 @@ public class HealthCondition {
                     break;
             }
         }while (select!=5);
-        System.out.println("good bye");
     }
 }
